b71914e619b39d9b016fe6f1590f854d360eee7a
TheSola10 <me@thesola.io>
2023-11-14 15:26:18 +0100

    applied review fixes

--- extension.js
+++ extension.js
@@ -8,18 +8,22 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
 export default class OverviewFlickExtension extends Extension
 {
-    gesture = new EdgeDragAction.EdgeDragAction(St.Side.RIGHT, Shell.ActionMode.NORMAL);
-
     callback() {
         Main.overview.show();
     }
 
     enable() {
-        this.gesture.connect('activated', this.callback.bind(this));
+        this.gesture = new EdgeDragAction.EdgeDragAction(St.Side.RIGHT, Shell.ActionMode.NORMAL);
+        this._gevent = this.gesture.connect('activated', this.callback.bind(this));
         global.stage.add_action(this.gesture);
     }
 
     disable() {
         global.stage.remove_action(this.gesture);
+        if (this._gevent) {
+            this.gesture.disconnect(this._gevent);
+            this._gevent = null;
+        }
+        this.gesture = null;
     }
 }
